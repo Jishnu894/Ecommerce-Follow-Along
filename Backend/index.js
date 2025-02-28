53416eae670cb02d1e50c3bf3a60fb42072442e0
Jishnu894 <vjishnukumar@gmail.com>
2025-02-28 13:21:02 +0530

    Achagit add .

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,27 @@
 const express=require('express');
 const connectDB = require('./src/Database/db');
+const ConnectDB = require('./src/Database/db');
 const app=express();
 
 require('dotenv').config({
-    path:'./src/config/.env'
-});
+    path:"./src/Database/db"
+})
 
 const PORT=process.env.port || 5000;
 const url=process.env.db_url;
-console.log(url)
+console.log(PORT)
 app.get('/',(req,res)=>{
     res.send('Hello World');
 })
 
 
-app.listen(PORT,async()=>{
 
-    try {
-        await connectDB(url);
-        console.log(`Server is running on port ${PORT}`);
+app.listen(PORT,async()=>{
+    try{
+        await ConnectDB(url);
+        console.log(`server is running on the port ${PORT}`);
     }
     catch(err){
-        console.log(err);
+        console.status(400).json({message:"servernot running",err})
     }
 });
--- Backend/src/Controllers/Products.js
+++ Backend/src/Controllers/Products.js
@@ -2,7 +2,10 @@ const {Router} = require('express');
 const { productupload } = require('../../multer');
 const productModel = require('../Model/Productmodel');
 const productrouter = Router();
-const path=require('path')
+const path=require('path');
+const userModel = require('../Model/userModel');
+const { equal } = require('assert');
+const { promises } = require('readline');
 
 productrouter.get("/get-product", async (req, res) => {
     try{
@@ -12,8 +15,10 @@ productrouter.get("/get-product", async (req, res) => {
             return res.status(400).json({message:"No products found"});
         }
         const products = productfind.map((product) => {
+
             return {
                 id: product._id,
+                
                 name: product.name,
                 description: product.description,
                 category: product.category,
@@ -34,6 +39,86 @@ productrouter.get("/get-product", async (req, res) => {
         console.log(err);
     }
 });
+
+productrouter.post('/cart', async(req,res)=>{
+    const {email, productid, quantity, productName} = req.body;
+    try{
+        if(!email){
+            return res.status(404).json({message:"Please fill all the fields"});
+        }
+        const findemail = await userModel.findOne({email:email});
+        if(!findemail){
+            return res.status(404).json({message:"Email not found"});
+            }
+            if(!mongoose.types.objectId.isvalid(productid)){
+                return res.status(404).json({message:"Invalid product id"});
+            }
+            if(!quantity && quantity<0){
+                return res.status(404).json({message:"you dont have enough quantity"});
+                }
+        const findproduct = await productModel.findById(productid);
+        if(!findproduct){
+            return res.status(404).json({message:"Product not found"});
+    }
+    const cartProduct = await findemail.cart.findIndex((i)=>{
+        return i.productid === productid
+    })
+    if(cartProduct>-1){
+        findemail.cart[cartProduct].quantity+=quantity;
+    }
+    else{
+        findemail.cart.push({productid, productName, quantity});
+
+    }
+
+}
+    catch(err){
+        console.log(err);
+    }
+})
+
+productrouter.get("/getcart", async(req, res)=>{
+    try{
+    const {email} = req.body;
+    if(!email){
+        return res.status(404).json({message:"user does not exist"});
+    }
+    const user = await userModel.findOne({email:email}).populate({
+        path: 'cart.productid',
+        model:productModel
+    })
+
+    if(!user){
+        return res.status(404).json({message:"User not found"});
+    }
+}
+catch(err){
+    console.log(err);
+}
+})
+
+productrouter.put('/edit-cart', async(req, res)=>{
+    const {email, productid, quantity} = req.body;
+
+    if(!email|| !productid||!quantity==undefined){
+        return res.status(404).json({message:"put all the details"});
+        }
+        const findemail = await userModel.findOne({email:email})
+        if(!finduser){
+            return res.status(404).json({message:"User is not found"});
+            }
+        const findproduct = await productModel.findOne({_id: productid})
+        if(!findproduct || findproduct.stock<=0){
+            return res.status(404).json({message:"Product is not found or out of stock"})
+            }
+
+    const findcartproduct=finduser.cart.find(item=>item.productid===productid)
+
+    if(findcartproduct){
+        finduser.quantity = quantity = quantity;
+    }
+    return res.status(200).json({message:"product updated"})
+})
 
 productrouter.post("/post-product",productupload.array('files'),async(req, res) => {
     const {name, description, category, tags, price, stock, email} = req.body;
